cabb38abff61c228fc67edde3eb1e94b98e7a2d9
SahilGupta-CN <sahilgupta9977@cloudnexus.in>
2025-08-31 14:24:37 +0530

    reactapp

---
+++ my-app/src/App.jsx
@@ -0,0 +1,7 @@
+export default function App() {
+  return (
+    <div className="min-h-screen flex items-center justify-center bg-gradient-to-r from-indigo-500 to-purple-600">
+      <h1 className="text-4xl font-bold text">Hello Tailwind + Vite + React 🚀</h1>
+    </div>
+  )
+}
